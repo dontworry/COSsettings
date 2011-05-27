ff16df59bce7cc0c82a466d37c8a6bc7a41cf29a
Serban Zodian <giant@kappa.ro>
2011-05-27 17:06:27 +0300

    cc

--- src/com/cyanogenmod/cmparts/activities/PerformanceSettingsActivity.java
+++ src/com/cyanogenmod/cmparts/activities/PerformanceSettingsActivity.java
@@ -37,6 +37,12 @@ import java.io.File;
  */
 public class PerformanceSettingsActivity extends PreferenceActivity implements Preference.OnPreferenceChangeListener {
 
+    private static final String COMPCACHE_PREF = "zram_size";
+
+    private static final String COMPCACHE_PERSIST_PROP = "persist.service.compcache";
+
+    private static final String COMPCACHE_DEFAULT = SystemProperties.get("persist.zram.size");
+   
     private static final String GENERAL_CATEGORY = "general_category";
 
     private static final String JIT_PREF = "pref_jit_mode";
@@ -60,6 +66,8 @@ public class PerformanceSettingsActivity extends PreferenceActivity implements P
     private static final String LOCK_MMS_PREF = "pref_lock_mms";
 
     private static final int LOCK_MMS_DEFAULT = 0;
+   
+    private ListPreference mCompcachePref;
 
     private CheckBoxPreference mJitPref;
 
@@ -68,6 +76,8 @@ public class PerformanceSettingsActivity extends PreferenceActivity implements P
     private ListPreference mHeapsizePref;
 
     private AlertDialog alertDialog;
+
+    private int swapAvailable = -1;
 
 
     @Override
@@ -80,6 +90,16 @@ public class PerformanceSettingsActivity extends PreferenceActivity implements P
         PreferenceScreen prefSet = getPreferenceScreen();
         
         PreferenceCategory generalCategory = (PreferenceCategory)prefSet.findPreference(GENERAL_CATEGORY);
+
+        mCompcachePref = (ListPreference) prefSet.findPreference(COMPCACHE_PREF);
+        if (isSwapAvailable()) {
+            if (SystemProperties.get(COMPCACHE_PERSIST_PROP) == "1")
+                SystemProperties.set(COMPCACHE_PERSIST_PROP, COMPCACHE_DEFAULT);
+            mCompcachePref.setValue(SystemProperties.get(COMPCACHE_PERSIST_PROP, COMPCACHE_DEFAULT));
+            mCompcachePref.setOnPreferenceChangeListener(this);
+        } else {
+            generalCategory.removePreference(mCompcachePref);
+        }
 
         mJitPref = (CheckBoxPreference) prefSet.findPreference(JIT_PREF);
         String jitMode = SystemProperties.get(JIT_PERSIST_PROP,
@@ -133,8 +153,24 @@ public class PerformanceSettingsActivity extends PreferenceActivity implements P
                 return true;
             }
         }
+        if (preference == mCompcachePref) {
+            if (newValue != null) {
+                SystemProperties.set(COMPCACHE_PERSIST_PROP, (String)newValue);
+                return true;
+            }
+        }
 
         return false;
     }
+
+    /**
+     * Check if swap support is available on the system
+     */
+       private boolean isSwapAvailable() {
+           if (swapAvailable < 0) {
+              swapAvailable = new File("/proc/swaps").exists() ? 1 : 0;
+           }
+           return swapAvailable > 0;
+       }
 
 }
