a8d642e3cc3eac13200787e51f403da611adbefd
Serban Zodian <giant@kappa.ro>
2011-06-18 08:49:32 +0300

    0 degree rotation for orientation locking: CMParts

--- src/com/cyanogenmod/cmparts/activities/UIActivity.java
+++ src/com/cyanogenmod/cmparts/activities/UIActivity.java
@@ -62,6 +62,8 @@ public class UIActivity extends PreferenceActivity implements OnPreferenceChange
 
     private static final String OVERSCROLL_WEIGHT_PREF = "pref_overscroll_weight";
 
+    private static final String ROTATION_0_PREF = "pref_rotation_0";
+
     private static final String ROTATION_90_PREF = "pref_rotation_90";
 
     private static final String ROTATION_180_PREF = "pref_rotation_180";
@@ -77,6 +79,8 @@ public class UIActivity extends PreferenceActivity implements OnPreferenceChange
     private ListPreference mOverscrollPref;
 
     private ListPreference mOverscrollWeightPref;
+
+    private CheckBoxPreference mRotation0Pref;
 
     private CheckBoxPreference mRotation90Pref;
 
@@ -128,11 +132,13 @@ public class UIActivity extends PreferenceActivity implements OnPreferenceChange
         mOverscrollWeightPref.setOnPreferenceChangeListener(this);
 
         /* Rotate 180 */
+        mRotation0Pref = (CheckBoxPreference) prefSet.findPreference(ROTATION_0_PREF);
         mRotation90Pref = (CheckBoxPreference) prefSet.findPreference(ROTATION_90_PREF);
         mRotation180Pref = (CheckBoxPreference) prefSet.findPreference(ROTATION_180_PREF);
         mRotation270Pref = (CheckBoxPreference) prefSet.findPreference(ROTATION_270_PREF);
         int mode = Settings.System.getInt(getContentResolver(),
-                        Settings.System.ACCELEROMETER_ROTATION_MODE, 5);
+                        Settings.System.ACCELEROMETER_ROTATION_MODE, 13);
+        mRotation0Pref.setChecked((mode & 8) != 0);
         mRotation90Pref.setChecked((mode & 1) != 0);
         mRotation180Pref.setChecked((mode & 2) != 0);
         mRotation270Pref.setChecked((mode & 4) != 0);
@@ -164,11 +170,16 @@ public class UIActivity extends PreferenceActivity implements OnPreferenceChange
             Settings.System.putInt(getContentResolver(), Settings.System.POWER_DIALOG_PROMPT,
                     value ? 1 : 0);
             return true;
-        } else if (preference == mRotation90Pref || preference == mRotation180Pref || preference == mRotation270Pref) {
+        } else if (preference == mRotation0Pref || mRotation90Pref || preference == mRotation180Pref || preference == mRotation270Pref) {
             int mode = 0;
+                if (mRotation0Pref.isChecked()) mode |= 8;
                 if (mRotation90Pref.isChecked()) mode |= 1;
                 if (mRotation180Pref.isChecked()) mode |= 2;
                 if (mRotation270Pref.isChecked()) mode |= 4;
+            if (mode == 0) {
+                mode |= 8;
+                mRotation0Pref.setChecked(true);
+            }
                     Settings.System.putInt(getContentResolver(), Settings.System.ACCELEROMETER_ROTATION_MODE, mode);
             return true;
         } 
